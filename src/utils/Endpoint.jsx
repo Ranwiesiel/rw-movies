5c54b8928da7175ce85dfc6c60ba0e5adbcaeaa3
Ranwiesiel <ronggofc@gmail.com>
2025-03-13 21:28:13 +0700

    cmon gk lucu :(

--- src/utils/Endpoint.jsx
+++ src/utils/Endpoint.jsx
@@ -1,3 +1,3 @@
-const BASE_API = 'http://imdb.sesatnime.serv00.net/api'
+const BASE_API = 'https://imdb.sesatnime.serv00.net/api'
 export const LIST_MOVIE = BASE_API + '/movies?limit=all'
 export const LIST_ANIME = BASE_API + '/tvs?limit=all'
